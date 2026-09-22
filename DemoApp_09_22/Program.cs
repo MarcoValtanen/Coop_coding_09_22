60fd0209e6e2329a36a7b0e761124d390462a13f
Reidar Nilsen <reidar.nilsen@zocom.>
2026-09-22 11:12:32 +0200

    Add project files.

---
+++ DemoApp_09_22/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DemoApp_09_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            SayHello("Reidar");
+
+            int result = Add(3, 3);
+            int.Parse("128");
+            Math.Max(11,12);
+            Console.WriteLine(result);
+        }
+
+        static void SayHello(string name)
+        {
+            Console.WriteLine($"Hej från Sverige {name}");
+        }
+
+        static int Add(int a, int b)
+        {
+            Console.WriteLine("Resuktatet blir: ");
+            return a + b;
+            
+        }
+    }
+}
