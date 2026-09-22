c4926d12da3aa695027fe91b9ffe7ef4a1578aa7
MarcoValtanen <marco@valtanen.se>
2026-09-22 14:28:35 +0200

    Färdigställt AgeCheckers

--- DemoApp_09_22/AgeChecker.cs
+++ DemoApp_09_22/AgeChecker.cs
@@ -8,7 +8,12 @@ namespace DemoApp_09_22
     {
         public void Run()
         {
-            Console.WriteLine("Elev 1 utvecklar denna");
+            Console.WriteLine("Vilket år är du född");
+            int currentYear = DateTime.Now.Year;
+            int birthdateYear = int.Parse(Console.ReadLine());
+            int ageYear = currentYear - birthdateYear;
+
+            Console.WriteLine($"Du är {ageYear} år gammal");
         }
     }
 }
